dd69f3af791f0d9494588b1e06afb9dcefdd878f
linxi-01 <129656406+linxi-01@users.noreply.github.com>
2024-10-22 23:51:18 +0800

    Create PCA_sklearn.py
    PCA接口调用

---
+++ 林希/week04/PCA_sklearn.py
@@ -0,0 +1,7 @@
+import numpy as np
+from sklearn.decomposition import PCA
+X = np.array([[-1,2,66,-1], [-2,6,58,-1], [-3,8,45,-2], [1,9,36,1], [2,10,62,1], [3,5,83,2]])  #导入数据，维度为4
+pca = PCA(n_components=2)  # 降到2维
+pca.fit(X)                 # 执行
+newX = pca.fit_transform(X)# 降维后的数据
+print(newX)                # 输出降维后的数据
