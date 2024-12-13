c5d4cacb7b0d997c509e9e5eb7c09080fdbdbdc6
alu1108 <252784724@qq.com>
2024-12-13 22:24:06 +0800

    Add files via upload

--- 颜鸿裕/week01/utils.py
+++ 颜鸿裕/week01/utils.py
@@ -1,6 +1,5 @@
 import matplotlib.image as mpimg
 import numpy as np
-import cv2
 import tensorflow as tf
 
 def load_image(path):
@@ -11,19 +10,21 @@ def load_image(path):
     crop_img = img[yy: yy + short_edge, xx: xx + short_edge]
     return crop_img
 
-
-def resize_image(image, size):
+def resize_image(image, size,
+                 method=tf.image.ResizeMethod.BILINEAR,
+                 align_corners=False):
     with tf.name_scope('resize_image'):
-        images = []
-        for i in image:
-            i = cv2.resize(i, size)
-            images.append(i)
-        images = np.array(images)
-        return images
+        image = tf.expand_dims(image, 0)
+        image = tf.image.resize_images(image, size,
+                                       method, align_corners)
+        image = tf.reshape(image, tf.stack([-1,size[0], size[1], 3]))
+        return image
 
-
-def print_answer(argmax):
-    with open("./data/model/index_word.txt", "r", encoding='utf-8') as f:
-        synset = [l.split(";")[1][:-1] for l in f.readlines()]
-
-    return synset[argmax]
+def print_prob(prob, file_path):
+    synset = [l.strip() for l in open(file_path).readlines()]
+    pred = np.argsort(prob)[::-1]
+    top1 = synset[pred[0]]
+    print(("Top1: ", top1, prob[pred[0]]))
+    top5 = [(synset[pred[i]], prob[pred[i]]) for i in range(5)]
+    print(("Top5: ", top5))
+    return top1
