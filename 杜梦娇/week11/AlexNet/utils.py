2f43ef9ace4ea96ddfe411c88b940a7920a29872
dumjiao <45990218+dumjiao@users.noreply.github.com>
2024-12-13 22:04:54 +0800

    Create utils.py

---
+++ 杜梦娇/week11/AlexNet/utils.py
@@ -0,0 +1,31 @@
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+from tensorflow.python.ops import array_ops
+import tensorflow as tf
+
+# 从给定路径加载图像
+def load_image(path):
+    # 读取图片，rgb
+    img = mpimg.imread(path)
+    # 将图片修剪成中心的正方形
+    short_edge = min(img.shape[:2])
+    yy = int((img.shape[0] - short_edge) / 2)
+    xx = int((img.shape[1] - short_edge) / 2) #裁剪图像为正方形
+    return crop_img
+
+# 图像数据扁平化
+def resize_image(image, size):
+    with tf.name_scope('resize_image'):
+        images = []
+        for i in image:
+            i = cv2.resize(i, size)
+            images.append(i)
+        images = np.array(images)
+        return images
+
+# 根据模型预测的索引argmax输出对应的标签:猫/狗
+def print_answer(argmax):
+    with open("./data/index_word.txt", "r", encoding='utf-8') as f:
+        synset = [l.split(";")[1][:-1] for l in f.readlines()]
+    return synset[argmax]
