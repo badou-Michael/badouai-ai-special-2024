45ece5e425247f2b946e1a1133be546c472595cc
chengyuanlin <chengyuanlin@linewell.com>
2024-12-19 15:58:47 +0800

    第十二周作业-resnet、inception、mobilenet

---
+++ 成元林/第十二周/mobileNetModel.py
@@ -0,0 +1,112 @@
+# -------------------------------------------------------------#
+#   MobileNet的网络部分
+# -------------------------------------------------------------#
+import warnings
+import numpy as np
+
+from keras.preprocessing import image
+
+from keras.models import Model
+from keras.layers import DepthwiseConv2D, Input, Activation, Dropout, Reshape, BatchNormalization, \
+    GlobalAveragePooling2D, GlobalMaxPooling2D, Conv2D
+from keras.applications.imagenet_utils import decode_predictions
+from keras import backend as K
+
+
+def MobileNet(input_shape=[224, 224, 3], depth_multiplier=1, dropout=1e-3, classes=1000):
+    img_input = Input(shape=input_shape)
+
+    # 224,224,3 -> 112,112,32
+    x = conv_block(img_input, 32, strides=(2, 2))
+
+    # 112,112,32 -> 112,112,64
+    x = depthwise_conv_block(x, 64, depth_multiplier, block_id=1)
+
+    # 112,112,64 -> 56,56,128
+    x = depthwise_conv_block(x, 128, depth_multiplier,
+                             strides=(2, 2), block_id=2)
+    # 56,56,128 -> 56,56,128
+    x = depthwise_conv_block(x, 128, depth_multiplier, block_id=3)
+
+    # 56,56,128 -> 28,28,256
+    x = depthwise_conv_block(x, 256, depth_multiplier,
+                             strides=(2, 2), block_id=4)
+
+    # 28,28,256 -> 28,28,256
+    x = depthwise_conv_block(x, 256, depth_multiplier, block_id=5)
+
+    # 28,28,256 -> 14,14,512
+    x = depthwise_conv_block(x, 512, depth_multiplier,
+                             strides=(2, 2), block_id=6)
+
+    # 14,14,512 -> 14,14,512
+    x = depthwise_conv_block(x, 512, depth_multiplier, block_id=7)
+    x = depthwise_conv_block(x, 512, depth_multiplier, block_id=8)
+    x = depthwise_conv_block(x, 512, depth_multiplier, block_id=9)
+    x = depthwise_conv_block(x, 512, depth_multiplier, block_id=10)
+    x = depthwise_conv_block(x, 512, depth_multiplier, block_id=11)
+
+    # 14,14,512 -> 7,7,1024
+    x = depthwise_conv_block(x, 1024, depth_multiplier,
+                             strides=(2, 2), block_id=12)
+    x = depthwise_conv_block(x, 1024, depth_multiplier, block_id=13)
+
+    # 7,7,1024 -> 1,1,1024
+    x = GlobalAveragePooling2D()(x)
+    x = Reshape((1, 1, 1024))(x)
+    x = Dropout(dropout)(x)
+    x = Conv2D(classes, (1, 1), padding='same')(x)
+    x = Activation('softmax')(x)
+    x = Reshape((classes,))(x)
+
+    inputs = img_input
+
+    model = Model(inputs, x, name='mobilenet_1_0_224_tf')
+    model_name = 'mobilenet_1_0_224_tf.h5'
+    model.load_weights(model_name)
+
+    return model
+
+
+def conv_block(inputs, filters, kernel=(3, 3), strides=(1, 1)):
+    x = Conv2D(filters, kernel, padding='same', use_bias=False, strides=strides, name='conv1')(inputs)
+    x = BatchNormalization()(x)
+    return Activation(relu6, name='conv1_relu')(x)
+
+
+def depthwise_conv_block(inputs, pointwise_conv_filters,
+                         depth_multiplier=1, strides=(1, 1), block_id=1):
+    x = DepthwiseConv2D((3, 3), padding='same', depth_multiplier=depth_multiplier, strides=strides, use_bias=False)(
+        inputs)
+    x = BatchNormalization()(x)
+    x = Activation(relu6)(x)
+    x = Conv2D(pointwise_conv_filters, (1, 1), padding='same', use_bias=False, strides=(1, 1))(x)
+    x = BatchNormalization()(x)
+    return Activation(relu6)(x)
+
+
+def relu6(x):
+    return K.relu(x, max_value=6)
+
+
+def preprocess_input(x):
+    x /= 255.
+    x -= 0.5
+    x *= 2.
+    return x
+
+
+if __name__ == '__main__':
+    model = MobileNet(input_shape=(224, 224, 3))
+    model.load_weights("mobilenet_1_0_224_tf.h5")
+    img_path = 'bike.jpg'
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    print('Input image shape:', x.shape)
+
+    preds = model.predict(x)
+    print(np.argmax(preds))
+    # 只显示top1
+    print('Predicted:', decode_predictions(preds, 1))
