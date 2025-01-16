f183bb021c99f691cfcf23bb53dfe8b1829501bc
chicheng662 <165747374+chicheng662@users.noreply.github.com>
2025-01-16 23:44:55 +0800

    Create week16-1.py

---
+++ 罗湘荣/week16/week16-1.py
@@ -0,0 +1,16 @@
+#实现yolov5
+import cv2
+import torch
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+img = cv2.imread('street.jpg')
+
+results = model(img)
+
+output_img = cv2.resize(results.render()[0],(512,512))
+print(output_img.shape)
+
+cv2.imshow('YOLOv5', output_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
